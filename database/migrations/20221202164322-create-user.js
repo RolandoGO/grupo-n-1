02cf29cebebd2a97c880014c605d36295e211c04
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-12-02 20:59:56 -0300

    creating user model and migration asociated with role model

---
+++ database/migrations/20221202164322-create-user.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      RoleId: {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:"Roles",
+          key:"id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { get } = require('../controllers/index')
 
 const router = express.Router()
 
 // example of a route with index controller get function
-router.get('/', get)
+router.get('/', (req,res)=> res.send("en pagina de inicio"))
+
 
 module.exports = router
