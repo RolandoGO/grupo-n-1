903b2c2497aed575d6df2a613c9f5d061b270405
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-12-02 21:16:57 -0300

    finish transaction model and migration and asociation with user and categorie

---
+++ database/models/transaction.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Transaction.belongsTo(models.User,{
+        foreignKey:"id"
+      })
+    }
+  };
+  Transaction.init({
+
+    description:DataTypes.STRING,
+    
+
+    amount: {
+      type:DataTypes.FLOAT,
+      allowNull:false},
+
+    userId: {
+      type:DataTypes.INTEGER,
+      allowNull:false},
+
+    categoryId: {
+      type:DataTypes.INTEGER,
+      allowNull:false},
+
+    date:{
+      type:DataTypes.DATE,
+      allowNull:false},
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+    paranoid:true
+  });
+  return Transaction;
+};
