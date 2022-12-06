ef2982329557bd47c41a4113e6dfe395e7492cd3
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-12-06 17:43:30 -0300

    create userRouter, and the method for getting all the users in index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,25 +1,27 @@
 const createHttpError = require('http-errors')
+const {User} = require("../database/models")
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
-  // get: catchAsync(async (req, res, next) => {
-  //   try {
-  //     const response = await Test.findAll()
-  //     endpointResponse({
-  //       res,
-  //       message: 'Test retrieved successfully',
-  //       body: response,
-  //     })
-  //   } catch (error) {
-  //     const httpError = createHttpError(
-  //       error.statusCode,
-  //       `[Error retrieving index] - [index - GET]: ${error.message}`,
-  //     )
-  //     next(httpError)
-  //   }
-  // })
+  getUsers: catchAsync(async (req, res, next) => {
+
+    try {
+      const response = await User.findAll({attributes:["firstName", "lastName", "email", "createdAt"]})
+      endpointResponse({
+        res,
+        message: `Users retrieved successfully, there are ${response.length} users in the database`,
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving index] - [index - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  })
 
   
 }
