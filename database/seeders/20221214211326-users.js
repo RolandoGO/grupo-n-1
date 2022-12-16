04ffe42b9dd09fc3e1552c062db6ede51013aebb
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-12-16 12:50:42 -0300

    refactoring code, addin fake user data array to the readme file, addin function for hashing fake password before seeden

--- database/seeders/20221214211326-users.js
+++ database/seeders/20221214211326-users.js
@@ -1,8 +1,19 @@
 'use strict';
-const {hashUserArray: userArray} = require("../fakeDataSeeders")
+const bcrypt = require('bcrypt');
 
+//The array of fake user date is in the README.md file
+//function for hashing passwords in the fake user data array.
+function fakePasswordEncrypt(array){
+    
+  const newArrray = array.map((obj)=>{
+     obj.password = bcrypt.hashSync(obj.password, 10)
+     return obj
 
-//seeder for populating the users table whit the fake data
+   })
+   return newArrray
+ }
+
+//seeder for populating the users table whit the fake user data
 //warning: You have to populate the roles table for before the users, and use the id of the roles for the users fake data in roleId propertie.
 module.exports = {
 
